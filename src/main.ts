ec7ac460643857d34ea6f5e59a60affbe8881ed4
b-ss <2764411477@qq.com>
2022-02-17 14:03:31 +0800

    add vant ui lib

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'amfe-flexible/index.js'
+// import 'amfe-flexible'
 import './assets/common.css'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <div><van-button type="primary">主要按钮</van-button></div>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -8,10 +9,12 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Button } from 'vant';
 
 @Options({
   components: {
     HelloWorld,
+    [Button.name]: Button
   },
 })
 export default class Home extends Vue {}
